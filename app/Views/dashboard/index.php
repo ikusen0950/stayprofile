deb1ca985369be9f894443dbe9fe5f937e1a1a4c
Hussain Habeeb <87605729+ikusen0950@users.noreply.github.com>
2025-10-28 17:07:30 +0600

    Update index.php

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -708,7 +708,7 @@
                         data-kt-scroll-wrappers="#kt_app_main" data-kt-scroll-offset="5px">
                         <!--begin::Sidebar menu-->
                         <div id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false"
-                            class="flex-column-fluid menu menu-sub-indention menu-column menu-rounded menu-active-bg mb-7 mt-5 mt-lg-2 mt-md-2">
+                            class="flex-column-fluid menu menu-sub-indention menu-column menu-rounded menu-active-bg mb-7 mt-10 mt-lg-2 mt-md-2">
 
                             <!--begin::Logo-->
                         <a href="/" class="app-sidebar-logo mt-4 mb-4">
