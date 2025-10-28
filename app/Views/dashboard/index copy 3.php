55b8ecc17ac588c3219a3835b1e2091bd43566b2
Hussain Habeeb <87605729+ikusen0950@users.noreply.github.com>
2025-10-28 12:15:01 +0600

    ne wpush

--- app/Views/dashboard/index copy 3.php
+++ app/Views/dashboard/index copy 3.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <meta name="theme-color" content="#1e1e2d" />
     <meta name="csrf-token" content="<?= csrf_hash() ?>" />
     <meta property="og:locale" content="en_US" />
